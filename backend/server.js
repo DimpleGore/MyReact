4e9c28f490bf076f13606e7790ed069ac0f6133c
Dimple Gore <dimplegore2000@gmail.com>
2021-07-22 12:59:14 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 var express = require('express')
 var cors = require('cors')
 require('dotenv').config()
+var mongoose = require('mongoose')
 
 var app = express()
 
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb+srv://Dimple:Dimplegore@975@dimple.llwb2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+     {
+       useCreateIndex: true,
+       useFindAndModify: false,
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     },
+     (err) => {
+       if (err) throw err;
+     }
+   );
+   
 
 var PORT = process.env.PORT || 5000
 app.use(require('./Routes/userRoutes'))
